8aace93d7a8a1a4cb7f54c205d9a88ae56bae054
abhinavpandey47 <abhinavpandeycr7@gmail.com>
2026-06-06 12:14:34 +0530

    valid anagram

---
+++ Summer_Assignment_2401921530008/Week_2/Day_1/validAnagram.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        String s1 = new String(arr1);
+        String t1 = new String(arr2);
+        return s1.equals(t1);
+    }
+}
