2cf280f78ab2c6a1417c6cdc209f699ab811f79d
abhinavpandey47 <abhinavpandeycr7@gmail.com>
2026-06-16 10:52:48 +0530

    Update linked_list_cycle.java

--- Summer_Assignment_2401921530008/Week_3/Day_1/linked_list_cycle.java
+++ Summer_Assignment_2401921530008/Week_3/Day_1/linked_list_cycle.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode s = head, f = head;
+        
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+            
+            if (s == f) return true;
+        }
+        
+        return false;
+    }
+}
