9d0e8829b2c41fadb19dfdc0073366a2b481cf58
abhinavpandey47 <abhinavpandeycr7@gmail.com>
2026-06-22 10:01:12 +0530

    Create next_greater_element.java

---
+++ Summer_Assignment_2401921530008/Week_3/Day_3/next_greater_element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        Map<Integer, Integer> mp = new HashMap<>();
+        
+        for (int n : nums2) {
+            while (!st.isEmpty() && st.peek() < n) {
+                mp.put(st.pop(), n);
+            }
+            st.push(n);
+        }
+        
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = mp.getOrDefault(nums1[i], -1);
+        }
+        
+        return ans;
+    }
+}
